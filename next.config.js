7c695dd3869ae58e69a20f67769be927d107e301
Anthony Chiu <kingychiu@gmail.com>
2022-04-23 14:49:25 +0800

    Add vercel-insights to the allowed source

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
+  default-src 'self' vitals.vercel-insights.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app vitals.vercel-insights.com;
   style-src 'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com;
   img-src * blob: data:;
   media-src 'none';
